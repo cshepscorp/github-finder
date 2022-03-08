beace33cc1987fa0e55a610b6656127777e7e5f3
cshepscorp <sheppard.christy@gmail.com>
2022-03-08 13:55:48 -0500

    https error

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -13,7 +13,7 @@ export const searchUsers = async (text) => {
   const params = new URLSearchParams({
     q: text,
   });
-  const response = await fetch(`http://api.github.com/search/users?${params}`);
+  const response = await fetch(`https://api.github.com/search/users?${params}`);
   const { items } = await response.json();
   return items;
 };
